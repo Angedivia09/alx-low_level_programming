62bbfc8d10912c3693da10581f52d2e0f87b26a8
ange divine <angedivine327@gmail.com>
2023-02-21 09:19:16 -0800

    .c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,27 @@
-#include "putchar.h"
-/**
- * main - Entry point
+/*
+ * File: 0-putchar.c
+ * Auth: Brennan D Baraban
  *
- * Return: Always 0 (Success)
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "alx" followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *sh = "putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
